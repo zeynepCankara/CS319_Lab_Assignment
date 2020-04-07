10384a5a660c944cceb6cf06c8b1c2d1f2ba7dd5
Zeynep Cankara <zeynep_cankara@windowslive.com>
2020-04-07 12:49:05 +0300

    Game is implemented in Main class.

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,34 @@ public class Main {
     ArrayOrganizer ao = new ArrayOrganizer();
     ArrayList<Integer> res = ao.concatAndSort(ArrayList1, ArrayList2, ArrayList3);
     System.out.println("Result => " + res);
+
+    Player tom = new Player("Tom");
+    Player john = new Player("John");
+    Player james = new Player("James");
+    Player henry = new Player("Henry");
+
+    Die die = new Die();
+
+    for(int i = 0; i < 5; i++){
+        int current_point = die.rollDie();
+        tom.addScore(current_point);
+    }
+    for(int i = 0; i < 5; i++){
+        int current_point = die.rollDie();
+        john.addScore(current_point);
+    }
+    for(int i = 0; i < 5; i++){
+        int current_point = die.rollDie();
+        james.addScore(current_point);
+    }
+    for(int i = 0; i < 5; i++){
+        int current_point = die.rollDie();
+        henry.addScore(current_point);
+    }
+
+    System.out.println("Tom has " + tom.getScore() + " points.");
+    System.out.println("Jonh has " + john.getScore() + " points.");
+    System.out.println("James has " + james.getScore() + " points.");
+    System.out.println("Henry has " + henry.getScore() + " points.");
     }
 }
